f64cab110ebe0863a2bc4c2925e95e6a1f2cbe4c
cyberRobotic <47058145+cyberRobotic@users.noreply.github.com>
2020-04-25 18:34:27 +0200

    Update connection.cpp

--- test-connection-Intel-Realsense-D435-Camera/connection.cpp
+++ test-connection-Intel-Realsense-D435-Camera/connection.cpp
@@ -1,1 +1,15 @@
+```cpp
+#include <librealsense2/rs.hpp>
+#include <librealsense2/hpp/rs_internal.hpp>
+#include <iostream>
 
+int main()
+{
+  rs2::context ctx;
+  std::cout <<"Hello from librealsense - " RS2_API_VERSION_STR << std::endl;
+  std::cout <<"You have " ctx.query_devices().size() << " Realsense devices connected" << std::endl;
+  
+  return 0;
+}
+  
+```
